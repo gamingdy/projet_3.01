ebc526b0289bb5e0a19324c0f502f7f99d299630
ShadowWarp <83580380+ShadowWarp@users.noreply.github.com>
2023-11-24 10:47:38 +0100

    feat: update rendezvous

--- src/model/rendezvous.class.php
+++ src/model/rendezvous.class.php
@@ -5,4 +5,26 @@
         private datetime $_dateHeureRDV;
         private int $_dureeMinutes = 30;
     }
+
+    public function __construct($_usager, $_medecin) {
+        $this->$_dateHeureRDV = $_dateHeureRDV;
+        $this->$_dureeMinutes = $_dureeMinutes;
+    }
+
+    public function getDateHeureRDV(): datetime {
+        return $this->_dateHeureRDV;
+    }
+
+    public function getDureeMinutes(): int {
+        return $this->_dureeMinutes;
+    }
+
+    public function getUsager(): Usager {
+        return $this->_usager;
+    }
+
+    public function getMedecin(): int {
+        return $this->_medecin;
+    }
+
 ?>
