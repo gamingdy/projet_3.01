b395e14dd167eaefc303c11f107be6e6ba584d46
Gamingdy <christgik12@gmail.com>
2023-11-24 11:00:48 +0100

    feat: création du constructeur Usager

--- src/model/usager.class.php
+++ src/model/usager.class.php
@@ -1,10 +1,20 @@
 <?php
-class Usager{
-    private int $_id;
+include 'individu.class.php';
+class Usager extends Individu {
+    private Individu $_individu;
     private string $_adresse;
     private datetime $_dateNaissance;
     private string $_lieuNaissance;
     private int $_securiteSociale;
     private Medecin $_medecinReferent;
+
+    public function __construct($_nom,$_prenom,$_civilite,$_adresse,$_dateNaissance,$_lieuNaissance,$_securiteSociale){
+        parent::__construct($_nom,$_prenom,$_civilite);
+        $this->$_adresse = $_adresse;
+        $this->$_dateNaissance = $_dateNaissance;
+        $this->$_lieuNaissance = $_lieuNaissance;
+        $this->$_securiteSociale = $_securiteSociale;
+    }
+
 }
-?>
+
