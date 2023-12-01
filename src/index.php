f4693225ee64f429a78f39391690992f7dc016d5
Gamingdy <christgik12@gmail.com>
2023-12-01 10:39:15 +0100

    feat: création du docker compose et le dockerfile

--- src/index.php
+++ src/index.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>test</title>
 </head>
 <body>
 <?php
 use Dotenv\Dotenv;
 
 require '../vendor/autoload.php';
-include 'model/usager.class.php';
-include 'model/civilite.class.php';
-$usager = new Usager("Doe","John",Civilite::M,"1 rue de la paix","01/01/2000","Paris",123456789);
+include "model/civilite.class.php";
+include 'model/individu.class.php';
+
+//$usager = new Usager("Doe","John",Civilite::M,"1 rue de la paix","01/01/2000","Paris",123456789);
+$usager = new Individu("Doe","John",Civilite::M);
 echo $usager->getNom();
 
 $dotenv = Dotenv::createImmutable(__DIR__,"../.env");
 $dotenv->safeLoad();
 $test = getenv('DB_HOST');
-var_dump($_ENV);
+//var_dump($_ENV);
 echo "\nui";
 ?>
 </body>
