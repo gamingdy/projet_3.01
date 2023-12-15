4fcf6a6350b99186b1b45fd9f0520d8480e28b22
Gamingdy <christgik12@gmail.com>
2023-12-15 10:26:50 +0100

    feat: ajout de la base de donné, ainsi que de la création automatique des tables

--- db_model/create.db.sql
+++ db_model/create.db.sql
@@ -1,38 +1,40 @@
-CREATE TABLE Individu(
-                         Id_Individu INT AUTO_INCREMENT,
-                         nom VARCHAR(50),
-                         prenom VARCHAR(50),
-                         civilite VARCHAR(50),
-                         PRIMARY KEY(Id_Individu)
+USE php_project;
+
+CREATE TABLE IF NOT EXISTS Individu(
+    Id_Individu INT AUTO_INCREMENT,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    civilite VARCHAR(50),
+    PRIMARY KEY(Id_Individu)
 );
 
-CREATE TABLE Médecin(
-                        Id INT AUTO_INCREMENT,
-                        Id_Individu INT NOT NULL,
-                        PRIMARY KEY(Id),
-                        FOREIGN KEY(Id_Individu) REFERENCES Individu(Id_Individu)
+CREATE TABLE IF NOT EXISTS Médecin(
+    Id INT AUTO_INCREMENT,
+    Id_Individu INT NOT NULL,
+    PRIMARY KEY(Id),
+    FOREIGN KEY(Id_Individu) REFERENCES Individu(Id_Individu)
 );
 
-CREATE TABLE Usager(
-                       Id_Usager INT AUTO_INCREMENT,
-                       adresse VARCHAR(50),
-                       dateNaissance DATE,
-                       lieuNaissance VARCHAR(50),
-                       securiteSociale VARCHAR(13),
-                       Id INT NOT NULL,
-                       Id_Individu INT NOT NULL,
-                       PRIMARY KEY(Id_Usager),
-                       FOREIGN KEY(Id) REFERENCES Médecin(Id),
-                       FOREIGN KEY(Id_Individu) REFERENCES Individu(Id_Individu)
+CREATE TABLE IF NOT EXISTS Usager(
+    Id_Usager INT AUTO_INCREMENT,
+    adresse VARCHAR(50),
+    dateNaissance DATE,
+    lieuNaissance VARCHAR(50),
+    securiteSociale VARCHAR(13),
+    Id INT NOT NULL,
+    Id_Individu INT NOT NULL,
+    PRIMARY KEY(Id_Usager),
+    FOREIGN KEY(Id) REFERENCES Médecin(Id),
+    FOREIGN KEY(Id_Individu) REFERENCES Individu(Id_Individu)
 );
 
-CREATE TABLE RendezVous(
-                           Id_RendezVous INT AUTO_INCREMENT,
-                           dateHeureRDV DATETIME,
-                           dureeMinutes INT,
-                           Id INT NOT NULL,
-                           Id_Usager INT NOT NULL,
-                           PRIMARY KEY(Id_RendezVous),
-                           FOREIGN KEY(Id) REFERENCES Médecin(Id),
-                           FOREIGN KEY(Id_Usager) REFERENCES Usager(Id_Usager)
+CREATE TABLE IF NOT EXISTS RendezVous(
+    Id_RendezVous INT AUTO_INCREMENT,
+    dateHeureRDV DATETIME,
+    dureeMinutes INT,
+    Id INT NOT NULL,
+    Id_Usager INT NOT NULL,
+    PRIMARY KEY(Id_RendezVous),
+    FOREIGN KEY(Id) REFERENCES Médecin(Id),
+    FOREIGN KEY(Id_Usager) REFERENCES Usager(Id_Usager)
 );
