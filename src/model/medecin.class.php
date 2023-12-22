dd8df103085e6fb1ae0db9bd39ec1584f82d2dbf
Gamingdy <christgik12@gmail.com>
2023-12-22 21:45:41 +0100

    feat: extend de Medecin vers Individu et ajout d'un setter et getter pour l'id

--- src/model/medecin.class.php
+++ src/model/medecin.class.php
@@ -1,12 +1,23 @@
 <?php
-class Medecin{
+
+require_once 'individu.class.php';
+
+class Medecin extends Individu{
     private int $_id;
 
 
-    public function __construct($_id)
-    {
-        $this->$_id = $_id;
+    public function __construct ($nom, $prenom, $civilite) {
+        parent::__construct($nom, $prenom, $civilite);
+    }
+
+    public function setId (int $_id): void {
+        $this->_id = $_id;
+    }
+
+    public function getId (): int {
+        return $this->_id;
     }
 
 }
+
 ?>
