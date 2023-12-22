7b3c91c23d6aad46e3f2173af598c93e07da3c2a
Gamingdy <christgik12@gmail.com>
2023-12-22 21:50:11 +0100

    feat: création de DaoIndividu pour gérer la communication controlleur <-> bdd

---
+++ src/dao/dao-individu.class.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/connexion.php';
+
+require_once __DIR__ . '/../model/individu.class.php';
+require_once __DIR__ . '/../model/civilite.class.php';
+
+
+class DaoIndividu{
+
+    private PDO $_pdo;
+
+    public function __construct () {
+        $this->_pdo = Connexion::getInstance()->getPDO();
+    }
+
+    public function getIndividu (int $_id): Individu | null {
+        $sql = "SELECT * FROM individu WHERE id = :id";
+        $stmt = $this->_pdo->prepare($sql);
+        $stmt->bindValue(':id', $_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($row) {
+            $civilite = Civilite::fromString($row['civilite']);
+            $individu = new Individu($row['nom'], $row['prenom'], $civilite);
+            $individu->setId($row['id']);
+            return $individu;
+        }
+        return null;
+    }
+
+    public function addIndividu (Individu $_individu): int {
+        $sql = "INSERT INTO individu (nom,prenom,civilite) VALUES (:nom,:prenom,:civilite)";
+        $stmt = $this->_pdo->prepare($sql);
+        $stmt->bindValue(':nom', $_individu->getNom());
+        $stmt->bindValue(':prenom', $_individu->getPrenom());
+        $stmt->bindValue(':civilite', $_individu->getCivilite()->value);
+        $stmt->execute();
+        return $this->_pdo->lastInsertId();
+    }
+}
