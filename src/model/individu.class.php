7ac7b0ff991ec385ecdef0f1b131deba1ee0457e
Gamingdy <christgik12@gmail.com>
2023-12-22 17:26:08 +0100

    feat: ajout d'un setId pour pouvoir définir l'identifiant après création de l'instance

--- src/model/individu.class.php
+++ src/model/individu.class.php
@@ -1,5 +1,6 @@
 <?php
-class Individu{    // Classe de composition pour les classes Usager et Medecin (suite à une récommandation de M. Gaëtan PIQUE)
+class Individu{
+    // Classe de composition pour les classes Usager et Medecin (suite à une récommandation de M. Gaëtan PIQUE)
     private Civilite $_civilite;
     private string $_nom;
     private string $_prenom;
@@ -25,5 +26,9 @@ class Individu{    // Classe de composition pour les classes Usager et Medecin (
 
     public function getId(): int {
         return $this->_id;
+    }
+
+    public function setId(int $_id): void {
+        $this->_id = $_id;
     }
 }
