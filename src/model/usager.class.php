6fa476f140dfa28832f3ee95f2bda2e28b4c5f43
Gamingdy <christgik12@gmail.com>
2023-12-22 21:10:07 +0100

    fix: initialisation du medecinReferent à nul pour les Usager

--- src/model/usager.class.php
+++ src/model/usager.class.php
@@ -1,41 +1,43 @@
 <?php
-include 'individu.class.php';
-class Usager extends Individu {
+require_once 'individu.class.php';
+
+class Usager extends Individu{
     private string $_adresse;
     private string $_dateNaissance;
     private string $_lieuNaissance;
     private int $_securiteSociale;
-    private Medecin $_medecinReferent;
+    private Medecin | null $_medecinReferent;
 
-    public function __construct($_nom,$_prenom,$_civilite,$_adresse,$_dateNaissance,$_lieuNaissance,$_securiteSociale){
-        parent::__construct($_nom,$_prenom,$_civilite);
+    public function __construct ($_nom, $_prenom, $_civilite, $_adresse, $_dateNaissance, $_lieuNaissance, $_securiteSociale) {
+        parent::__construct($_nom, $_prenom, $_civilite);
         $this->_adresse = $_adresse;
         $this->_dateNaissance = $_dateNaissance;
         $this->_lieuNaissance = $_lieuNaissance;
         $this->_securiteSociale = $_securiteSociale;
+        $this->_medecinReferent = null;
     }
-    
-    public function getAdresse(): string {
+
+    public function getAdresse (): string {
         return $this->_adresse;
     }
-    
-    public function getDateNaissance(): datetime {
+
+    public function getDateNaissance (): string {
         return $this->_dateNaissance;
     }
-    
-    public function getLieuNaissance(): string {
+
+    public function getLieuNaissance (): string {
         return $this->_lieuNaissance;
     }
-    
-    public function getSecuriteSociale(): int {
+
+    public function getSecuriteSociale (): int {
         return $this->_securiteSociale;
     }
 
-    public function getMedecinReferent(): Medecin {
+    public function getMedecinReferent (): Medecin | null {
         return $this->_medecinReferent;
     }
 
-    public function setMedecinReferent(Medecin $_medecinReferent): void {
+    public function setMedecinReferent (Medecin $_medecinReferent): void {
         $this->_medecinReferent = $_medecinReferent;
     }
 
