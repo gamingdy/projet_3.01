623f0d9198db94f81dbf909e77974afd980d3d56
Gamingdy <christgik12@gmail.com>
2023-12-22 09:39:16 +0100

    fix: suppression des "$"

--- src/model/usager.class.php
+++ src/model/usager.class.php
@@ -1,19 +1,18 @@
 <?php
 include 'individu.class.php';
 class Usager extends Individu {
-    private Individu $_individu;
     private string $_adresse;
-    private datetime $_dateNaissance;
+    private string $_dateNaissance;
     private string $_lieuNaissance;
     private int $_securiteSociale;
     private Medecin $_medecinReferent;
 
     public function __construct($_nom,$_prenom,$_civilite,$_adresse,$_dateNaissance,$_lieuNaissance,$_securiteSociale){
         parent::__construct($_nom,$_prenom,$_civilite);
-        $this->$_adresse = $_adresse;
-        $this->$_dateNaissance = $_dateNaissance;
-        $this->$_lieuNaissance = $_lieuNaissance;
-        $this->$_securiteSociale = $_securiteSociale;
+        $this->_adresse = $_adresse;
+        $this->_dateNaissance = $_dateNaissance;
+        $this->_lieuNaissance = $_lieuNaissance;
+        $this->_securiteSociale = $_securiteSociale;
     }
     
     public function getAdresse(): string {
