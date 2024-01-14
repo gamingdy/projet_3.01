3fba7e88f811a8b17f946b113ca51c48b5e8d8ec
Gamingdy <christgik12@gmail.com>
2024-01-14 21:02:57 +0100

    feat: séparation dateheurerdv en 2 pour permettre de rechercher des rdv à une date

--- db_model/create.db.sql
+++ db_model/create.db.sql
@@ -28,11 +28,13 @@ CREATE TABLE IF NOT EXISTS usager (
 
 CREATE TABLE IF NOT EXISTS rendezvous (
     id           INT AUTO_INCREMENT,
-    dateheurerdv DATETIME,
+    date_rdv     DATE,
+    heure_debut  TIME,
     dureeminutes INT,
     id_medecin   INT NOT NULL,
     id_usager    INT NOT NULL,
     PRIMARY KEY (id),
+    UNIQUE KEY unique_rdv (date_rdv, heure_debut, id_medecin, id_usager),
     FOREIGN KEY (id_medecin) REFERENCES medecin (id),
     FOREIGN KEY (id_usager) REFERENCES usager (id)
 );
@@ -47,8 +49,14 @@ VALUES ('Doe', 'Jane', 'Mme');
 INSERT INTO individu(nom, prenom, civilite)
 VALUES ('Doe', 'Jack', 'M');
 
+INSERT INTO individu(nom, prenom, civilite)
+VALUES ('Sofia', 'kartoshka', 'Mme');
+
 INSERT INTO medecin(id_individu)
 VALUES (1);
+
+INSERT INTO medecin(id_individu)
+VALUES (4);
 
 INSERT INTO usager(adresse, datenaissance, lieunaissance, securitesociale, id_medecin, id_individu)
 VALUES ('1 rue de la paix', '1990-01-01', 'Paris', '1234567890123', 1, 2);
@@ -56,5 +64,11 @@ VALUES ('1 rue de la paix', '1990-01-01', 'Paris', '1234567890123', 1, 2);
 INSERT INTO usager(adresse, datenaissance, lieunaissance, securitesociale, id_medecin, id_individu)
 VALUES ('2 rue de la paix', '1990-01-01', 'Paris', '1234567890123', NULL, 3);
 
-INSERT INTO rendezvous(dateheurerdv, dureeminutes, id_medecin, id_usager)
-VALUES ('01/01/2024 10:00:00', 30, 1, 2);
+INSERT INTO rendezvous(date_rdv, heure_debut, dureeminutes, id_medecin, id_usager)
+VALUES ('2019-01-01', '10:00:00', 30, 1, 2);
+
+INSERT INTO rendezvous(date_rdv, heure_debut, dureeminutes, id_medecin, id_usager)
+VALUES ('2019-01-01', '10:30:00', 30, 2, 2);
+
+INSERT INTO rendezvous(date_rdv, heure_debut, dureeminutes, id_medecin, id_usager)
+VALUES ('2019-01-02', '11:00:00', 30, 1, 1);
