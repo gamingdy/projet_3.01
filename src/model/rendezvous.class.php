c5719c5cc1e5aa45aaef7165a4ee820da1131c2d
Gamingdy <christgik12@gmail.com>
2024-01-14 17:30:37 +0100

    fix: correction d'un problème avec les attribut de la classe  RendezVous

--- src/model/rendezvous.class.php
+++ src/model/rendezvous.class.php
@@ -1,30 +1,40 @@
 <?php
-    class RendezVous{
-        private Usager $_usager;
-        private Medecin $_medecin;
-        private datetime $_dateHeureRDV;
-        private int $_dureeMinutes = 30;
-    
 
-    public function __construct($_usager, $_medecin, $_dateHeureRDV, $_dureeMinutes) {
-        $this->$_dateHeureRDV = $_dateHeureRDV;
-        $this->$_dureeMinutes = $_dureeMinutes;
+require_once __DIR__ . '/usager.class.php';
+require_once __DIR__ . '/medecin.class.php';
+
+class RendezVous{
+    private int $id;
+    private Usager $_usager;
+    private Medecin $_medecin;
+    private string $_dateHeureRDV;
+    private int $_dureeMinutes = 30;
+
+
+    public function __construct ($_usager, $_medecin, $_dateHeureRDV, $_dureeMinutes) {
+        $this->_usager = $_usager;
+        $this->_medecin = $_medecin;
+        $this->_dateHeureRDV = $_dateHeureRDV;
+        $this->_dureeMinutes = $_dureeMinutes;
     }
 
-    public function getDateHeureRDV(): datetime {
+    public function getDateHeureRDV (): string {
         return $this->_dateHeureRDV;
     }
 
-    public function getDureeMinutes(): int {
+    public function getDureeMinutes (): int {
         return $this->_dureeMinutes;
     }
 
-    public function getUsager(): Usager {
+    public function getUsager (): Usager {
         return $this->_usager;
     }
 
-    public function getMedecin(): int {
+    public function getMedecin (): Medecin {
         return $this->_medecin;
     }
+
+    public function setId (int $id): void {
+        $this->id = $id;
     }
-?>
+}
